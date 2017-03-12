c567d872ab497270bace1f855138db84768e5cb9
Coding <coding@macbook-pro.local>
2017-03-12 21:00:32 +0800

    Initial Commit

---
+++ Fractal/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fractal
+//
+//  Created by Coding on 12/03/2017.
+//  Copyright © 2017 Coding. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
